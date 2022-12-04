5f4723b56d5867a32ad880f5a165f3029e65405e
jesuanp <jespaterm@gmail.com>
2022-12-04 14:48:36 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function App() {
+
+  const [origin, setOrigin] = React.useState({
+    latitude: 10.921065,
+    longitude: -74.785781,
+  })
+
+  const [destination, setDestination] = React.useState({
+    latitude: 10.917750,
+    longitude: -74.795256,
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 10.917692,
+          longitude: -74.799231,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+
+        <Marker
+          coordinate={origin}
+        />
+
+        <Marker
+          coordinate={destination}
+        />
+
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={'AIzaSyCCpn70ZJEIvFYTsUyxArbhmtFJXoNgtgo'}
+        />
+
+      </MapView>
+
     </View>
   );
 }
@@ -16,5 +56,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
